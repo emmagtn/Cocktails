8fdaf00f5e772e40fca2b21f32b20d7e58e28b00
emmagtn <emmalilygaetan@gmail.com>
2024-04-06 12:25:10 +0200

    emma tested funny things

---
+++ evil.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.header("SupBitches")
+if st.button("click here for a surprise"):
+    st.write ("muehehehehe you've summoned the evil drunk cocktail drinker. BEWARE")
+    st.button("oh no what will you do now?")
+    st.write("run or fight...? will you be a coward or win the drunk battle of death?")
+    st.button('run')
+
+st.header("I've given up on this test now thanks for coming")
+
+
