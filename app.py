870bc900c1fd8f2ae6d07aea9f556aa11f47abd5
Billyscodingclass <william.bloor@student.unisg.ch>
2024-04-06 14:10:14 +0200

    API done

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import pandas as pd
+import requests
+from io import StringIO
+
+# Function to load data from a URL
+def load_data(url):
+    # Assuming the CSV is accessible at a URL
+    response = requests.get(url)
+    csv_data = StringIO(response.text)
+    df = pd.read_csv(csv_data)
+    return df
+
+# URL of the CSV file (Update with the actual URL)
+csv_url = 'https://github.com/OzanGenc/CocktailAnalysis/raw/main/cocktails.csv'
+
+# Load data
+df = load_data(csv_url)
+
+# Streamlit UI
+st.title('Cocktail Finder')
+
+
+
 
 st.header("Helklllo")
 
