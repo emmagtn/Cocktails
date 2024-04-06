cdc0c433f5b4b948dd0aaefb53f9ac1a0fc84edc
emmagtn <emmalilygaetan@gmail.com>
2024-04-06 14:35:00 +0200

    title test & my favorites folder created

---
+++ title_page.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header ("Cocktails On The Go")
+st.write("Welcome to our App! Are you a student, just like us, struggling to find a cool new drink idea to test on a Sprittwoch? You've landed in the right place!")
+st.write("On our app, you are welcome to create your own Liquor Cabinet, which will in turn give you your top 5 best options for cocktails")
