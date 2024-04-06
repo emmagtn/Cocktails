73f8e7ee753bf844cbf623d37b9afb058a5f3f97
ladina1108 <ladina.neubert@student.unisg.ch>
2024-04-06 12:31:54 +0200

    "copy paste from main to my branch"

---
+++ appy 2nd.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.header("Helklllo")
+
+st. button("hello")
+
+st. button("Yannick")
+
+st. button("yoyostreak")
+
+st.button("Click here Willy")
+st.markdown("Hi there")
+
+
