e6abb1b630c53042faeb6f2550eac3177649cbb3
emmagtn <emmalilygaetan@gmail.com>
2024-04-06 12:00:31 +0200

    edit

--- emmatest.py
+++ emmatest.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header("SupBitches")
 if st.button("click here for a surprise"):
-    st.write "muehehehehe you've summoned the evil drunk cocktail drinker. BEWARE"
+    st.write ("muehehehehe you've summoned the evil drunk cocktail drinker. BEWARE")
